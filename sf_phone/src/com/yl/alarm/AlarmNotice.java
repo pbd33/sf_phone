d702bc1f1a6535008399b55b4f91ab5ad3bb2f89
Administrator <1667891919@qq.com>
2015-01-18 13:11:23 +0800

    1

--- sf_phone/src/com/yl/alarm/AlarmNotice.java
+++ sf_phone/src/com/yl/alarm/AlarmNotice.java
@@ -4,7 +4,6 @@ package com.yl.alarm;
 import android.location.Location;
 public class AlarmNotice {
 	
-	//比较当前监控终端的位置与上一次得到的监控终端的位置，如果上次的位置为null
 	public static boolean compare(AlarmBean bean,Location location){
 		if(bean.getSpeed()<1){
 			return false;
